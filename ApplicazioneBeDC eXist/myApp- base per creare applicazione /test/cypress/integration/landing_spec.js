171f347160ad53d4e4222e504332502c608841db
santapellino <s.pellino1@studenti.unipi.it>
2022-01-25 10:03:28 +0100

    mod

--- ApplicazioneBeDC eXist/myApp- base per creare applicazione /test/cypress/integration/landing_spec.js
+++
@@ -1,9 +0,0 @@
-/* global cy */
-describe('The landing page', function () {
-  it('should load ', function () {
-    cy.visit('/exist/yes/BDC/index.html')
-      .get('.alert')
-      .contains('app.xql')
-  })
-
-})
